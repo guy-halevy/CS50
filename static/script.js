af963b6ae78e0d19491fe9f01eaf3cc5c788954f
guy-halevy <148886533+guy-halevy@users.noreply.github.com>
2025-05-19 18:30:21 +0000

    עדכון בקובץ HTML

--- static/script.js
+++ static/script.js
@@ -103,16 +103,3 @@ document.addEventListener('DOMContentLoaded', function() {
 window.onload = function() {
   document.getElementById('autoSubmitForm').submit();
 };
-
-
-/* gets the list of countries/cities/regions from Google and auto complete the user input */
-function initMap() {
-  let autocomplete;
-  const countryRestrict = { country: [] };
-  autocomplete = new google.maps.places.Autocomplete(document.getElementById("autocomplete"),{componentRestrictions: countryRestrict,fields: ["geometry"],},);
-  window.initMap = initMap;}
-(g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})({key: "AIzaSyB41DRUbKWJHPxaFjMAwdrzWzbVKartNGg", v: "weekly"});
-
-
-
-
